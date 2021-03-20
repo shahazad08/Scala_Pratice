0109a057177372a7dd8e869ca99b584c66c7ec20
shahazad08 <sk.shahazad@gmail.com>
2021-03-19 18:23:03 -0800

    Modified: Unwanted comments removed

--- src/main/scala/example/MultidimensionalArray.scala
+++ src/main/scala/example/MultidimensionalArray.scala
@@ -4,29 +4,7 @@ import scala.Array.ofDim
 
 object MultidimensionalArray extends App {
 
-//  var arr1=Array.range(2,15)
-//  for(i<-arr1) {
-//    println(i)
-//  }
 
- // Multidimenstional Arrays
-// var myMatrix=ofDim[Int](3,3)
-//
-//  // build the matrix
-//  for(i<-0 to 2) {
-//    for(j<-0 to 2) {
-//      myMatrix(i)(j)=j
-//    }
-//
-//    // Print the Dimensional Matrix
-//    for(i<-0 to 2) {
-//      for(j<-0 to 2) {
-//        print(" "+myMatrix(i)(j))
-//      }
-//      println()
-//
-//    }
-//  }
 var myMatrix = ofDim[Int](3,3)
 
   // build a matrix
