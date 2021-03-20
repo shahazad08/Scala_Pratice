7e5e148095d3cd6fb853020a1ade9bcae93c318c
shahazad08 <sk.shahazad@gmail.com>
2021-03-19 18:52:09 -0800

    Added: Collection File is added

--- src/main/scala/example/Collections_List.scala
+++ src/main/scala/example/Collections_List.scala
@@ -1,5 +1,26 @@
 package example
 
-object Collections_List {
+object Collections_List extends App {
+  // List of Strings
+  val fruit: List[String] = List("apples", "oranges", "pears")
+
+  // List of Integers
+  val nums: List[Int] = List(1, 2, 3, 4)
+
+  // Empty List.
+  val empty: List[Nothing] = List()
+
+  // Two dimensional list
+  val dim: List[List[Int]] =
+    List(
+      List(1, 0, 0),
+      List(0, 1, 0),
+      List(0, 0, 1)
+    )
+
+  println("Fruit List", fruit)
+  println("nums List", nums)
+  println("empty List", empty)
+  println("dim List", dim)
 
 }
